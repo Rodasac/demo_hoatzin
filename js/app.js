b0bfaaef62ffd0c340fc5b5b5f648e85693728b5
Adolfo Bastardo <adolfo.bastardo@gmail.com>
2016-09-16 01:31:15 -0400

    Terminando primera parte de Proyectos

--- js/app.js
+++ js/app.js
@@ -177,3 +177,15 @@ $(document).ready(function() {
         }
     });
 });
+
+$('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+});
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $('.grid').isotope({ filter: filterValue });
+});
