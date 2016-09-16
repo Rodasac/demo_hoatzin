8935e77a2890f9eeb917ef5171ff41f40949dda5
Adolfo Bastardo <adolfo.bastardo@gmail.com>
2016-09-15 23:33:30 -0400

    Terminando primera parte de Servicios

--- js/app.js
+++ js/app.js
@@ -158,3 +158,22 @@ $(document).ready(function(){
 //         clearItems($(".hulnav"), $(scrolled).attr("id"));
 //     }
 // });
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
